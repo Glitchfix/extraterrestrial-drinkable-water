18331913fbc3f858c3e6fc46ed93830f8a7d79df
Shivanjan <schakravorty846@gmail.com>
2023-10-25 02:53:58 +0530

    start the world

---
+++ main.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import pandas as pd
+
+# Define a deep learning model to classify water safety
+
+
+class WaterSafetyClassifier(nn.Module):
+    def __init__(self, input_size, hidden_sizes, num_classes):
+        super(WaterSafetyClassifier, self).__init__()
+        layers = []
+        for i in range(len(hidden_sizes) - 1):
+            layers.append(nn.Linear(hidden_sizes[i], hidden_sizes[i + 1]))
+            layers.append(nn.ReLU())
+            layers.append(nn.BatchNorm1d(hidden_sizes[i + 1]))
+            layers.append(nn.Dropout(p=0.2))
+        layers.append(nn.Linear(hidden_sizes[-1], num_classes))
+        layers.append(nn.Sigmoid())
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+# Hyperparameters
+# Number of input features (based on the parameters in the dataset)
+input_size = 7
+# Increased hidden sizes for complex representations
+hidden_sizes = [256, 512, 256, 128]
+num_classes = 2  # Binary classification for drinkable and undrinkable water
+
+# Data preprocessing (replace with your dataset)
+data = pd.read_csv("water_samples.csv")  # Load the dataset from the CSV file
+# Implement data preprocessing steps, such as handling missing values and feature scaling
+
+# Create an instance of the model
+model = WaterSafetyClassifier(input_size, hidden_sizes, num_classes)
+
+# Define the loss function and optimizer
+# BCELoss is suitable for binary classification tasks like ours
+criterion = nn.BCELoss()
+# Adam optimizer is known for its effectiveness in deep learning training scenarios
+# Adjusted learning rate for stability and convergence
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Sample training loop (replace with actual training data and loop)
+# Train the model using the chosen criterion and optimizer
+for epoch in range(1000):
+    optimizer.zero_grad()
+    # Replace with the appropriate input and target tensors from your dataset
+    sample_input = torch.randn(1, input_size)
+    # Assuming a binary target label
+    target_label = torch.tensor([[0, 1]], dtype=torch.float)
+
+    output = model(sample_input)
+    loss = criterion(output, target_label)
+    loss.backward()
+    optimizer.step()
+    if (epoch + 1) % 100 == 0:
+        print(f'Epoch [{epoch + 1}/1000], Loss: {loss.item():.4f}')
+
+# Sample inference (replace with actual data for prediction)
+# Replace with an actual sample input for prediction
+sample_input = torch.randn(1, input_size)
+predicted_output = model(sample_input)
+print(f'Predicted output: {predicted_output}')
