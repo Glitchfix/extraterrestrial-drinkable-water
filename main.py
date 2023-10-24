641ce59220d64664823b9a2f13a0096c8d54d2e4
Shivanjan <schakravorty846@gmail.com>
2023-10-25 03:32:02 +0530

    accuracy: 0.63
    Signed-off-by: Shivanjan <schakravorty846@gmail.com>

--- main.py
+++ main.py
@@ -1,68 +1,79 @@
+import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import pandas as pd
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
 
-# Define a deep learning model to classify water safety
+# Read the dataset
+data = pd.read_csv('water.csv')
+data = shuffle(data)  # Shuffling the dataset
 
+# Extract the input features and labels
+X = data.drop('Potability', axis=1).values
+y = data['Potability'].values
 
-class WaterSafetyClassifier(nn.Module):
-    def __init__(self, input_size, hidden_sizes, num_classes):
-        super(WaterSafetyClassifier, self).__init__()
-        layers = []
-        for i in range(len(hidden_sizes) - 1):
-            layers.append(nn.Linear(hidden_sizes[i], hidden_sizes[i + 1]))
-            layers.append(nn.ReLU())
-            layers.append(nn.BatchNorm1d(hidden_sizes[i + 1]))
-            layers.append(nn.Dropout(p=0.2))
-        layers.append(nn.Linear(hidden_sizes[-1], num_classes))
-        layers.append(nn.Sigmoid())
-        self.model = nn.Sequential(*layers)
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Convert the data into PyTorch tensors
+X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
+y_train_tensor = torch.tensor(y_train, dtype=torch.long)
+X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
+y_test_tensor = torch.tensor(y_test, dtype=torch.long)
+
+# Define the model architecture and hyperparameters
+class WaterClassifier(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(WaterClassifier, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        return self.model(x)
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
 
+input_size = 9  # Adjust according to the number of input features
+hidden_size = 64  # You can adjust this based on experimentation
+output_size = 2  # Assuming binary classification for drinkable vs. undrinkable water
 
-# Hyperparameters
-# Number of input features (based on the parameters in the dataset)
-input_size = 9
-# Increased hidden sizes for complex representations
-hidden_sizes = [256, 512, 512, 256, 128]
-num_classes = 2  # Binary classification for drinkable and undrinkable water
+# Initialize the model
+model = WaterClassifier(input_size, hidden_size, output_size)
 
-# Data preprocessing (replace with your dataset)
-# Replace with the path to your dataset
-data = pd.read_csv("water.csv")
-# Implement data preprocessing steps, such as handling missing values and feature scaling
+# Define the loss function and the optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.0000001)  # You can adjust the learning rate as needed
 
-# Create an instance of the model
-model = WaterSafetyClassifier(input_size, hidden_sizes, num_classes)
+# Training loop
+num_epochs = 1000  # Define the appropriate number of epochs
+for epoch in range(num_epochs):
+    inputs = X_train_tensor
+    labels = y_train_tensor
 
-# Define the loss function and optimizer
-# BCELoss is suitable for binary classification tasks like ours
-criterion = nn.BCELoss()
-# Adam optimizer is known for its effectiveness in deep learning training scenarios
-# Adjusted learning rate for stability and convergence
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+    # Forward pass
+    outputs = model(inputs)
+    loss = criterion(outputs, labels)
 
-# Sample training loop (replace with actual training data and loop)
-# Train the model using the chosen criterion and optimizer
-for epoch in range(1000):
+    # Backward pass and optimization
     optimizer.zero_grad()
-    # Replace with the appropriate input and target tensors from your dataset
-    sample_input = torch.randn(1, input_size)
-    # Assuming a binary target label
-    target_label = torch.tensor([[0, 1]], dtype=torch.float)
-
-    output = model(sample_input)
-    loss = criterion(output, target_label)
     loss.backward()
     optimizer.step()
-    if (epoch + 1) % 100 == 0:
-        print(f'Epoch [{epoch + 1}/1000], Loss: {loss.item():.4f}')
 
-# Sample inference (replace with actual data for prediction)
-# Replace with an actual sample input for prediction
-sample_input = torch.randn(1, input_size)
-predicted_output = model(sample_input)
-print(f'Predicted output: {predicted_output}')
+    # Print the loss at every 100 epochs or as needed
+    if (epoch + 1) % 100 == 0:
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+# Evaluate the model
+with torch.no_grad():
+    outputs = model(X_test_tensor)
+    _, predicted = torch.max(outputs, 1)
+    total = y_test_tensor.size(0)
+    correct = (predicted == y_test_tensor).sum().item()
+    accuracy = correct / total
+    print(f'Accuracy on the test set: {accuracy:.2f}')
+
+# Save the trained model
+torch.save(model.state_dict(), 'water_classifier_model.model')
